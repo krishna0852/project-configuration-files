f43014abef0af6e14fa7267dc661abea49cad7cf
Krishna Teja Palla <63608191+krishna0852@users.noreply.github.com>
2024-02-17 14:28:52 +0530

    Update online- getprivateips.py, added a method to check name and ip address are already exists or not

--- getprivateips.py
+++ getprivateips.py
@@ -5,13 +5,14 @@ sess=boto3.Session(aws_access_key_id='',
                 aws_secret_access_key='',
                 region_name='ap-southeast-2')
 
+filename="/home/ubuntu/project-configuration-files/inventory-host"
 ec2=sess.resource("ec2")
 
 
 def getPrivateipOnTags():
     instance_names=['master','pract-cluster-spot-nodes-Node'] 
     for instance_name in instance_names:
-          print('filtering started')
+          print('filtering started: ')
           instances=ec2.instances.filter(Filters=[
             {
              'Name': 'tag:Name',
@@ -30,15 +31,28 @@ def getPrivateipOnTags():
           
 
 def putipInToHostFile(name, ipaddress):
-    # print(name)
-    # print(ipaddress)
-    filename='inventory-host'
-    print('opening file')
-    with open(filename, 'a') as file:
-       print('writing into file')
-       file.write(f"[{name}]\n{ipaddress}\n")
-
-
+    print(name)
+    print(ipaddress)
+    
+    check=checkIpAndNameexists(name, ipaddress)
+    #filename='host'
+    if check:
+        print("name and ip address already exists, skipping ..")      
+       
+    else: 
+        print ('opening file')
+        with open (filename, 'a') as file:
+            print('writing into file')
+            file.write(f"[{name}]\n{ipaddress}\n")
+        
+        
+def checkIpAndNameexists(name, ipaddress):
+    with open (filename, 'r') as file: 
+        getFileContent=file.read()
+        if name in getFileContent and ipaddress in getFileContent:
+            return True 
+        else:
+            return False
 
 call=getPrivateipOnTags()
 
